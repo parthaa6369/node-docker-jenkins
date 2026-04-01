34235a7664a43886039621f9172d509d5f46aa60
parthaa 56 <parthaa6369@gmail.com>
2026-04-01 17:34:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello ilam !')
+  response.send('Hello paridhi !')
 })
 
 app.listen(app.get('port'), function() {
