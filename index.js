27c6b5674d70d5fed0281f71e18a2eb180767681
parthaa 56 <parthaa6369@gmail.com>
2026-04-02 12:09:23 +0530

    Change response message in index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Webhook is triggered!')
+  response.send('hello vishnu!')
 })
 
 app.listen(app.get('port'), function() {
